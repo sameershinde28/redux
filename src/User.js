f6206a10db7a1e234c7a4ba739b1eef9216b3180
sameer <sameershinde@gmail.com>
2023-05-05 12:49:58 +0530

    props explanation calling/using

--- src/User.js
+++ src/User.js
@@ -4,11 +4,13 @@ const User=(props)=>{
     console.log(props); 
     console.log(props.data);
     console.log(props.data.name);
+
+    const {data}=props
     return(
         <div>
             <h1>User Component </h1>
             <h1>NAme:: {props.data.name}</h1>
-            <h1>Age:: {props.data.age}</h1>
+            <h1>Age:: {data.age}</h1>
             <h1>Address::    {props.data.address}</h1>
         </div>
     )
