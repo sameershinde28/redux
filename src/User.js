80c9fcd4f78496438a57e48963986f4d97eaa0c5
sameer <sameershinde@gmail.com>
2023-05-05 12:04:26 +0530

    display props

--- src/User.js
+++ src/User.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const User=(props)=>{
-    console.log(props);
+    console.log(props); 
     console.log(props.data);
     console.log(props.data.name);
     return(
         <div>
             <h1>User Component </h1>
-            <h1>{props.data.name}</h1>
+            <h1>NAme:: {props.data.name}</h1>
+            <h1>Age:: {props.data.age}</h1>
+            <h1>Address::    {props.data.address}</h1>
         </div>
     )
 }
